6b135f81be3e78dafa825207e91ceec3656b920c
Raghib Khan <khan03raghib@gmail.com>
2023-12-17 03:33:17 +0530

    Player,Camera Scripts

---
+++ Camera_Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camLookat : MonoBehaviour
+{
+    public Transform player, cameraTrans;
+
+    void Update()
+    {
+        cameraTrans.LookAt(player);
+    }
+}
+
